de62d9c3b26eac8d4b00b495379a7e047b5bcafb
Dedan Irungu <dedanirungu@gmail.com>
2023-08-15 10:24:18 +0300

    Changes to files: Entities/Attendant.php Entities/Destination.php Entities/Ticket.php Entities/TicketLog.php

--- Entities/TicketLog.php
+++ Entities/TicketLog.php
@@ -44,6 +44,8 @@ class TicketLog extends BaseModel
      */
     public function fields(Blueprint $table): void
     {
+        $this->fields = $table ?? new Blueprint($this->table);
+        
         $this->fields->increments('id')->html('text');
         $this->fields->foreignId('ticket_id')->html('recordpicker')->table(['queuing', 'ticket']);
         $this->fields->foreignId('attendant_id')->html('recordpicker')->table(['queuing', 'attendant']);
