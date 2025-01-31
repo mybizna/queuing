a4dad26783eeabf97aacfceb1874ae0ae4073aa4
Dedan <dedanirungu@gmail.com>
2025-01-31 20:41:22 +0300

    Remove uncessary routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Queuing\Http\Controllers\TicketController;
 
-
-Route::get('/queuing/ticket', 'TicketController@ticket')->name('queuing_ticket_api')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-
+Route::get('/queuing/ticket', [TicketController::class, 'ticket'])->name('queuing_ticket_api')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Queuing\Http\Controllers\TicketController;
 
-Route::get('/queuing/ticket', 'TicketController@ticket')->name('queuing_ticket')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::get('/queuing/ticket/create', 'TicketController@create')->name('queuing_ticket_create')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::get('/queuing/ticket/fetch', 'TicketController@ticket_fetch')->name('queuing_ticket_fetch')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::match (['get', 'post'], '/queuing/ticket/save', 'TicketController@save')->name('queuing_ticket_save');
-Route::match (['get', 'post'], '/queuing/ticket/move', 'TicketController@move')->name('queuing_ticket_move');
-Route::post('/queuing/ticket/savemove', 'TicketController@savemove')->name('queuing_ticket_savemove');
+
+Route::get('/queuing/ticket', [TicketController::class,'@ticket'])->name('queuing_ticket')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::get('/queuing/ticket/create', [TicketController::class,'create'])->name('queuing_ticket_create')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::get('/queuing/ticket/fetch', [TicketController::class,'ticket_fetch'])->name('queuing_ticket_fetch')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match (['get', 'post'], '/queuing/ticket/save', [TicketController::class,'save'])->name('queuing_ticket_save');
+Route::match (['get', 'post'], '/queuing/ticket/move', [TicketController::class,'move'])->name('queuing_ticket_move');
+Route::post('/queuing/ticket/savemove', [TicketController::class,'savemove'])->name('queuing_ticket_savemove');
